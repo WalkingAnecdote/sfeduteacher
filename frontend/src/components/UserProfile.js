fe2e72f5f6d661396a42f67cef5ef9e142613ca9
Andrey Rozhkov <tagan96_andrey@mail.ru>
2022-06-06 23:05:20 +0300

    Add profile page layout.

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,20 +1,29 @@
 import * as React from 'react';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import {
-    CssBaseline, Drawer, Box, AppBar, Toolbar, List,
-    Typography, Divider, IconButton, Container,
-    ListItemButton, ListItemIcon, ListItemText, Button
+    Typography, TextField, Paper
 } from '@mui/material';
-import {
-    ChevronLeft, People, Chat as ChatIcon, Menu
-} from '@mui/icons-material';
-import { Copyright, Chat } from '../components';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export const UserProfile = () => {
+    const user = useSelector(state => state.user)
+    const title = React.useMemo(() => {
+        if (user?.roles.includes('teacher')) {
+            return 'Профиль преподавателя'
+        } else if (user?.roles.includes('student')) {
+            return 'Профиль студента'
+        } else {
+            return 'Ваш профиль'
+        }
+    }, [user?.roles])
     return (
         <>
-            <Typography variant='h4' textAlign='center' style={{ marginBottom: '30px' }}>Ваш профиль</Typography>
+            <Typography variant='h4' textAlign='center' style={{ marginBottom: '30px' }}>{title}</Typography>
+            <Paper style={{ padding: '30px 20px' }}>
+                <TextField label="Фамилия" defaultValue={user?.middle_name} style={{ marginBottom: '20px'}} disabled fullWidth/>
+                <TextField label="Имя" defaultValue={user?.first_name} style={{ marginBottom: '20px'}} disabled fullWidth/>
+                <TextField label="Отчество" defaultValue={user?.last_name} style={{ marginBottom: '20px'}} disabled fullWidth/>
+                <TextField label="Почта" defaultValue={user?.email} disabled fullWidth/>
+            </Paper>
         </>
     )
 }
