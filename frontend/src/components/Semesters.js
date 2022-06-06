431b36c6c614c57f00b8587ed7bd90b02c691dee
Andrey Rozhkov <tagan96_andrey@mail.ru>
2022-06-07 01:22:29 +0300

    Add semester selection logic.

--- frontend/src/components/Semesters.js
+++ frontend/src/components/Semesters.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux'
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton, OutlinedInput, InputAdornment, Box, Fab, Typography, List, ListItem, ListItemText, TextField, Button } from '@mui/material';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton, OutlinedInput, InputAdornment, Box, Fab, Typography, List, ListItem, ListItemText, ListItemButton, TextField, Button } from '@mui/material';
 import { Close, Add, Delete } from '@mui/icons-material';
 import { BaseModal } from './Modal'
 
@@ -11,7 +11,7 @@ export const Semesters = () => {
     const semestersList = useSelector(state => state.semesters.semestersList)
     const [selectedGroup, setSelectedGroup] = React.useState(null)
     const [semesterModal, setSemesterModal] = React.useState(false)
-
+    const [selectedSemester, setSelectedSemester] = React.useState(null)
     const handleSubmitSemester = (event) => {
       event.preventDefault();
       const formData = new FormData(event.currentTarget);
@@ -38,7 +38,7 @@ export const Semesters = () => {
           {selectedGroup !== null ? (
             <OutlinedInput
               type='text'
-              value={selectedGroup.name}
+              value={`Выбранная группа: ${selectedGroup.name}`}
               fullWidth
               endAdornment={
                 <InputAdornment position="end">
@@ -78,55 +78,72 @@ export const Semesters = () => {
             </TableContainer>
           )}
           <>
-            {selectedGroup !== null && (
-              <>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', m: 1 }}>
-                  Семестры группы: 
-                  <Fab color="primary" aria-label="add" onClick={() => setSemesterModal(true)}>
-                    <Add />
-                  </Fab>
-                </Box>
-                {semestersList?.data?.length ? (
-                  <List sx={{ bgcolor: 'background.paper' }}>
-                    {semestersList?.data.map((semester) => (
-                      <ListItem
-                        key={semester.number}
-                        secondaryAction={
-                          <IconButton aria-label="delete" onClick={() => dispatch.semesters.asyncDeleteSemester({groupId: selectedGroup.id, semesterId: semester.id})}>
-                            <Delete />
-                          </IconButton>
-                        }
+            {selectedGroup !== null && (selectedSemester !== null ? (
+                <OutlinedInput
+                  type='text'
+                  value={`Выбранный семестр: ${selectedSemester.number}`}
+                  fullWidth
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={() => setSelectedSemester(null)}
                       >
-                        <ListItemText primary={`Семестр номер ${semester.number}`} />
-                      </ListItem>
-                    ))}
-                  </List>
-                ) : (
-                  <Typography>Семестры для группы пока не созданы.</Typography>
-                )}
-                <BaseModal open={semesterModal} setOpen={setSemesterModal}>
-                  <Box component="form" onSubmit={handleSubmitSemester} noValidate sx={{ mt: 1 }}>
-                      <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name='number'
-                        type='number'
-                        label='Номер семестра'
-                        autoFocus
-                      />
-                    <Button
-                      type="submit"
-                      fullWidth
-                      variant="contained"
-                      sx={{ mt: 3, mb: 2 }}
-                    >
-                      Создать сущность
-                    </Button>
+                        <Close />
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              ) : (
+                <>
+                  <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', m: 1 }}>
+                    Семестры группы: 
+                    <Fab color="primary" aria-label="add" onClick={() => setSemesterModal(true)}>
+                      <Add />
+                    </Fab>
                   </Box>
-                </BaseModal>
-              </>
-            )}
+                  {semestersList?.data?.length ? (
+                    <List sx={{ bgcolor: 'background.paper' }}>
+                      {semestersList?.data.sort((a, b) => a.number - b.number).map((semester) => (
+                        <ListItem
+                          key={semester.number}
+                          secondaryAction={
+                            <IconButton aria-label="delete" onClick={() => dispatch.semesters.asyncDeleteSemester({groupId: selectedGroup.id, semesterId: semester.id})}>
+                              <Delete />
+                            </IconButton>
+                          }
+                        >
+                          <ListItemButton onClick={() => setSelectedSemester(semester)}>
+                            <ListItemText primary={`Семестр номер ${semester.number}`} />
+                          </ListItemButton>
+                        </ListItem>
+                      ))}
+                    </List>
+                  ) : (
+                    <Typography>Семестры для группы пока не созданы.</Typography>
+                  )}
+                  <BaseModal open={semesterModal} setOpen={setSemesterModal}>
+                    <Box component="form" onSubmit={handleSubmitSemester} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                          margin="normal"
+                          required
+                          fullWidth
+                          name='number'
+                          type='number'
+                          label='Номер семестра'
+                          autoFocus
+                        />
+                      <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                      >
+                        Создать сущность
+                      </Button>
+                    </Box>
+                  </BaseModal>
+                </>
+            ))}
         </>
       </>
     )
