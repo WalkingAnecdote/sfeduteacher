eda898bafb8886d0a6311b16ae74f3d9cf507b37
Andrey Rozhkov <tagan96_andrey@mail.ru>
2022-06-09 00:15:27 +0300

    Update profile fields by user type.

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -2,19 +2,32 @@ import * as React from 'react';
 import {
     Typography, TextField, Paper
 } from '@mui/material';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const UserProfile = () => {
+    const dispatch = useDispatch()
     const user = useSelector(state => state.user)
+    const group = useSelector(state => state.groups.group)
+    const isTeacher = React.useMemo(() => {
+        return user?.roles.includes('teacher')
+    }, [user?.roles])
+    const isStudent = React.useMemo(() => {
+        return user?.roles.includes('student')
+    }, [user?.roles])
     const title = React.useMemo(() => {
-        if (user?.roles.includes('teacher')) {
+        if (isTeacher) {
             return 'Профиль преподавателя'
-        } else if (user?.roles.includes('student')) {
+        } else if (isStudent) {
             return 'Профиль студента'
         } else {
             return 'Ваш профиль'
         }
-    }, [user?.roles])
+    }, [isStudent, isTeacher])
+    React.useEffect(() => {
+        if (isStudent && group === null) {
+            dispatch.groups.asyncGetGroupById(user?.profile?.group_id)
+        }
+    }, [dispatch.groups, group, isStudent, user?.profile?.group_id])
     return (
         <>
             <Typography variant='h4' textAlign='center' style={{ marginBottom: '30px' }}>{title}</Typography>
@@ -22,7 +35,9 @@ export const UserProfile = () => {
                 <TextField label="Фамилия" defaultValue={user?.middle_name} style={{ marginBottom: '20px'}} disabled fullWidth/>
                 <TextField label="Имя" defaultValue={user?.first_name} style={{ marginBottom: '20px'}} disabled fullWidth/>
                 <TextField label="Отчество" defaultValue={user?.last_name} style={{ marginBottom: '20px'}} disabled fullWidth/>
-                <TextField label="Почта" defaultValue={user?.email} disabled fullWidth/>
+                <TextField label="Почта" defaultValue={user?.email} disabled style={{ marginBottom: '20px'}} fullWidth/>
+                {isTeacher && <TextField label="Дожность"  defaultValue={user?.profile?.rank || 'Должность не задана'} disabled fullWidth/>}
+                {isStudent && <TextField label="Группа" defaultValue={group.name} disabled fullWidth/>}
             </Paper>
         </>
     )
